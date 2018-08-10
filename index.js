de0bde0cadd5391752069d8684ae55ef1e21bf48
Amit Solanki <amit.fash@gmail.com>
2018-08-10 12:25:04 +0530

    fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports.default = (url, options) => {
   let timeout = 20;
   if (options && options.timeout) timeout = options.timeout;
-  const win = window.open(window.location.host, "_blank");
+  const win = window.open(window.location.href, "_blank");
   setTimeout(() => (win.location.href = url), timeout);
 };
